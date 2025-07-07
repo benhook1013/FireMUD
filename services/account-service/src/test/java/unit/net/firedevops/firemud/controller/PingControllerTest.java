94f93b0f7082d670894cca7d93c923f9c237c3c9
Ben Hook <ben.hook@hotmail.com>
2025-07-07 18:08:28 +1200

    test: use MockitoBean instead of deprecated MockBean

--- services/account-service/src/test/java/unit/net/firedevops/firemud/controller/PingControllerTest.java
+++ services/account-service/src/test/java/unit/net/firedevops/firemud/controller/PingControllerTest.java
@@ -9,7 +9,7 @@ import net.firedevops.firemud.service.PingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(PingController.class)
@@ -17,7 +17,7 @@ class PingControllerTest {
 
   @Autowired private MockMvc mockMvc;
 
-  @MockBean private PingService pingService;
+  @MockitoBean private PingService pingService;
 
   @Test
   void pingEndpointReturnsPong() throws Exception {
