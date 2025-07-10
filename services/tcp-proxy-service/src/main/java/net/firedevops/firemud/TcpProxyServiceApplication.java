76571d728f622a2932d40cad82ef483a9c6be174
Ben Hook <ben.hook@hotmail.com>
2025-07-10 17:29:11 +1200

    tcp-proxy: add initial telnet server

--- services/tcp-proxy-service/src/main/java/net/firedevops/firemud/TcpProxyServiceApplication.java
+++ services/tcp-proxy-service/src/main/java/net/firedevops/firemud/TcpProxyServiceApplication.java
@@ -1,11 +1,31 @@
 package net.firedevops.firemud;
 
+import jakarta.annotation.PreDestroy;
+import net.firedevops.firemud.telnet.TelnetServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class TcpProxyServiceApplication {
+  private final TelnetServer telnetServer;
+
+  public TcpProxyServiceApplication(TelnetServer telnetServer) {
+    this.telnetServer = telnetServer;
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(TcpProxyServiceApplication.class, args);
+  }
+
+  @EventListener(ApplicationReadyEvent.class)
+  public void startServer() throws InterruptedException {
+    telnetServer.start();
+  }
+
+  @PreDestroy
+  public void stopServer() {
+    telnetServer.stop();
   }
 }
