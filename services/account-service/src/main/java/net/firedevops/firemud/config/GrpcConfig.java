e04e8422b26c5c3a06bad633a06c199718967388
Ben Hook <ben.hook@hotmail.com>
2025-07-12 05:48:03 +1200

    Add JWT authentication interceptors

--- services/account-service/src/main/java/net/firedevops/firemud/config/GrpcConfig.java
+++ services/account-service/src/main/java/net/firedevops/firemud/config/GrpcConfig.java
@@ -5,6 +5,7 @@ import io.opentelemetry.api.trace.Tracer;
 import net.firedevops.firemud.common.grpc.LoggingInterceptor;
 import net.firedevops.firemud.common.grpc.MetricsInterceptor;
 import net.firedevops.firemud.common.grpc.TracingInterceptor;
+import net.firedevops.firemud.security.GrpcJwtAuthInterceptor;
 import org.lognet.springboot.grpc.GRpcGlobalInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,5 +28,12 @@ public class GrpcConfig {
   @GRpcGlobalInterceptor
   public TracingInterceptor tracingInterceptor(Tracer tracer) {
     return new TracingInterceptor(tracer);
+  }
+
+  @Bean
+  @GRpcGlobalInterceptor
+  public GrpcJwtAuthInterceptor grpcJwtAuthInterceptor(
+      net.firedevops.firemud.common.security.JwtUtil jwtUtil) {
+    return new GrpcJwtAuthInterceptor(jwtUtil);
   }
 }
