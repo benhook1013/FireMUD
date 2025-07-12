e3ca87fe02be5f4e6082c52286ae907f8e033265
Ben Hook <ben.hook@hotmail.com>
2025-07-13 03:26:14 +1200

    Add admin operations saga

--- services/logging-admin-service/src/main/java/net/firedevops/firemud/LoggingAdminServiceApplication.java
+++ services/logging-admin-service/src/main/java/net/firedevops/firemud/LoggingAdminServiceApplication.java
@@ -5,11 +5,14 @@ import io.swagger.v3.oas.annotations.info.Info;
 import net.firedevops.firemud.common.config.CommonAutoConfiguration;
 import net.firedevops.firemud.common.config.DatabaseAutoConfiguration;
 import net.firedevops.firemud.config.AuthConfig;
+import net.firedevops.firemud.config.GrpcClientProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GrpcClientProperties.class)
 @OpenAPIDefinition(info = @Info(title = "Logging Admin Service", version = "v1"))
 @Import({DatabaseAutoConfiguration.class, CommonAutoConfiguration.class, AuthConfig.class})
 public class LoggingAdminServiceApplication {
