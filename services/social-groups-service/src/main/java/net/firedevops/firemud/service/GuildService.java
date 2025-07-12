bd6710ceacf6daf2289680138696cc97f9e6da64
Ben Hook <ben.hook@hotmail.com>
2025-07-12 15:15:39 +1200

    Add guild member management

--- services/social-groups-service/src/main/java/net/firedevops/firemud/service/GuildService.java
+++ services/social-groups-service/src/main/java/net/firedevops/firemud/service/GuildService.java
@@ -1,11 +1,14 @@
 package net.firedevops.firemud.service;
 
+import net.firedevops.firemud.dto.AddGuildMemberRequest;
 import net.firedevops.firemud.dto.AddGuildStorageItemRequest;
 import net.firedevops.firemud.dto.CreateAllianceRequest;
 import net.firedevops.firemud.dto.CreateGuildRequest;
 import net.firedevops.firemud.dto.GuildAllianceDto;
 import net.firedevops.firemud.dto.GuildDto;
+import net.firedevops.firemud.dto.GuildMemberDto;
 import net.firedevops.firemud.dto.GuildStorageItemDto;
+import net.firedevops.firemud.dto.UpdateGuildMemberRoleRequest;
 
 public interface GuildService {
   GuildDto createGuild(CreateGuildRequest request);
@@ -13,4 +16,10 @@ public interface GuildService {
   GuildStorageItemDto addStorageItem(AddGuildStorageItemRequest request);
 
   GuildAllianceDto createAlliance(CreateAllianceRequest request);
+
+  GuildMemberDto addMember(AddGuildMemberRequest request);
+
+  GuildMemberDto updateMemberRole(UpdateGuildMemberRoleRequest request);
+
+  void removeMember(long tenantId, long guildId, long accountId);
 }
