d5dd237c34deb8c0767e51fc154e56b0c6213948
Ben Hook <ben.hook@hotmail.com>
2025-07-12 14:55:22 +1200

    fix accessibility issues

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -12,45 +12,47 @@ function App() {
 
   return (
     <>
-      <div>
+      <header>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <Button variant="outlined" onClick={() => setMode('game')} sx={{ mr: 1 }}>
-        Game Editor
-      </Button>
-      <Button
-        variant="outlined"
-        onClick={() => setMode('script')}
-        sx={{ mr: 1 }}
-      >
-        Script Editor
-      </Button>
-      <Button variant="outlined" onClick={() => setMode('demo')}>
-        Demo
-      </Button>
-      <div className="card">
-        {mode === 'game' && <GameEditor />}
-        {mode === 'script' && <ScriptEditor />}
-        {mode === 'demo' && (
-          <>
-            <Button variant="contained" onClick={() => setCount((c) => c + 1)}>
-              count is {count}
-            </Button>
-            <p>
-              Edit <code>src/App.tsx</code> and save to test HMR
-            </p>
-          </>
-        )}
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </header>
+      <main>
+        <h1>Vite + React</h1>
+        <Button variant="outlined" onClick={() => setMode('game')} sx={{ mr: 1 }}>
+          Game Editor
+        </Button>
+        <Button
+          variant="outlined"
+          onClick={() => setMode('script')}
+          sx={{ mr: 1 }}
+        >
+          Script Editor
+        </Button>
+        <Button variant="outlined" onClick={() => setMode('demo')}>
+          Demo
+        </Button>
+        <div className="card">
+          {mode === 'game' && <GameEditor />}
+          {mode === 'script' && <ScriptEditor />}
+          {mode === 'demo' && (
+            <>
+              <Button variant="contained" onClick={() => setCount((c) => c + 1)}>
+                count is {count}
+              </Button>
+              <p>
+                Edit <code>src/App.tsx</code> and save to test HMR
+              </p>
+            </>
+          )}
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </main>
     </>
   );
 }
