50bccc572074707e00b1e412f35d26795812ee5c
Ben Hook <ben.hook@hotmail.com>
2025-07-13 13:57:44 +1200

    Add saga checks to session start/stop

--- services/game-session-service/src/main/java/net/firedevops/firemud/config/GrpcClientConfig.java
+++ services/game-session-service/src/main/java/net/firedevops/firemud/config/GrpcClientConfig.java
@@ -2,6 +2,8 @@ package net.firedevops.firemud.config;
 
 import javax.net.ssl.SSLException;
 import net.firedevops.firemud.client.GameLogicClient;
+import net.firedevops.firemud.client.WorldManagementClient;
+import net.firedevops.firemud.client.EntityManagementClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +17,21 @@ public class GrpcClientConfig {
       GrpcClientProperties properties)
       throws SSLException {
     return new GameLogicClient(endpoints, properties);
+  }
+
+  @Bean
+  public WorldManagementClient worldManagementClient(
+      net.firedevops.firemud.common.config.ServiceEndpointsProperties endpoints,
+      GrpcClientProperties properties)
+      throws SSLException {
+    return new WorldManagementClient(endpoints, properties);
+  }
+
+  @Bean
+  public EntityManagementClient entityManagementClient(
+      net.firedevops.firemud.common.config.ServiceEndpointsProperties endpoints,
+      GrpcClientProperties properties)
+      throws SSLException {
+    return new EntityManagementClient(endpoints, properties);
   }
 }
