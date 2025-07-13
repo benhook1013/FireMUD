4bf9f6f7144c9cb019343a6717b642b99931c74f
Ben Hook <ben.hook@hotmail.com>
2025-07-13 17:21:31 +1200

    feat: instrument services with metrics

--- services/social-groups-service/src/main/java/net/firedevops/firemud/service/impl/PingServiceImpl.java
+++ services/social-groups-service/src/main/java/net/firedevops/firemud/service/impl/PingServiceImpl.java
@@ -1,5 +1,6 @@
 package net.firedevops.firemud.service.impl;
 
+import io.micrometer.core.annotation.Timed;
 import net.firedevops.firemud.common.LoggingUtil;
 import net.firedevops.firemud.service.PingService;
 import org.slf4j.Logger;
@@ -10,6 +11,7 @@ public class PingServiceImpl implements PingService {
   private static final Logger logger = LoggingUtil.getLogger(PingServiceImpl.class);
 
   @Override
+  @Timed(value = "social.ping")
   public String ping() {
     logger.info("Ping called");
     return "pong";
