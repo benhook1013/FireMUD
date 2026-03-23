501b501799cb955bb3abbcf663e3347275d26d0e
Ben Hook <ben.hook@hotmail.com>
2026-03-23 22:28:25 +1300

    Wire saga repositories in social groups service

---
+++ services/social-groups-service/src/main/java/net/firedevops/firemud/socialgroups/repository/SagaStepRepository.java
@@ -0,0 +1,11 @@
+package net.firedevops.firemud.socialgroups.repository;
+
+import java.util.List;
+import net.firedevops.firemud.common.saga.persistence.SagaStep;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SagaStepRepository extends JpaRepository<SagaStep, Long> {
+  List<SagaStep> findByInstanceId(Long instanceId);
+}
