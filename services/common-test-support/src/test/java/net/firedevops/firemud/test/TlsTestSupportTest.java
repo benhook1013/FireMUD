ac2f6de1c2c6f50b52ba55d18d007213f8eb3d2c
Ben Hook <5009589+benhook1013@users.noreply.github.com>
2026-09-20 02:00:48 +1200

    Simplify TLS fixture premature-close assertion

--- services/common-test-support/src/test/java/net/firedevops/firemud/test/TlsTestSupportTest.java
+++ services/common-test-support/src/test/java/net/firedevops/firemud/test/TlsTestSupportTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import reactor.netty.http.client.PrematureCloseException;
 
 class TlsTestSupportTest {
   @Test
@@ -19,23 +20,12 @@ class TlsTestSupportTest {
 
   @Test
   void recognizesPrematureCloseExceptionDirectlyAndInCauseChain() {
-    Throwable prematureCloseException = prematureCloseExceptionTestInstance();
+    Throwable prematureCloseException = PrematureCloseException.TEST_EXCEPTION;
     assertThat(TlsTestSupport.isTlsHandshakeRejection(prematureCloseException)).isTrue();
     assertThat(
             TlsTestSupport.isTlsHandshakeRejection(
                 new IllegalStateException("client request failed", prematureCloseException)))
         .isTrue();
-  }
-
-  private static Throwable prematureCloseExceptionTestInstance() {
-    try {
-      return (Throwable)
-          Class.forName("reactor.netty.http.client.PrematureCloseException")
-              .getField("TEST_EXCEPTION")
-              .get(null);
-    } catch (ReflectiveOperationException exception) {
-      throw new AssertionError("Reactor Netty TEST_EXCEPTION is unavailable", exception);
-    }
   }
 
   @ParameterizedTest
